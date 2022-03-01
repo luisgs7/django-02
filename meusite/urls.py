8fed6c44fc297e792a53c1d87edf6ba4972516ea
Luís Gustavo <luisgsilva270@gmail.com>
2022-03-01 17:25:06 +0000

    url home

--- meusite/urls.py
+++ meusite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('sobre/', include('sobre.urls')),
+    path('', include('home.urls'))
 ]
